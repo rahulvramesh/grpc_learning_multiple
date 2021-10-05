556c52b45ccbabbea6664a4dfd737ee977dc5d3e
Rahul V Ramesh <rahulvramesh@hotmail.com>
2021-10-05 13:13:41 +0700

    python run time

--- python-handler/handler.py
+++ python-handler/handler.py
@@ -5,25 +5,27 @@
 import logging
 import bidirectional_client as grpc_client
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 # 1, Define the JSON Structure to store rutime information
 # 2, Generate a request payload
 
 """ Runtimes Information """
 runtimes: dict = {
-    "python3":"localhost:50051" # Should Make The Port Constant
+    "python3": "localhost:50051"  # Should Make The Port Constant
 }
 
 
 """ Request Payload , Let's create a global payload structure"""
 request_payload: dict = {
     "runtime": "python3",
-    "action" : "run",
+    "action": "run",
     "payload": "hello",
-    "sid" : "ABC_788_GHGH" # For Sandboxing each request
+    "sid": "ABC_788_GHGH"  # For Sandboxing each request
 }
 
-def InvokeRuntime(payload : dict) -> bool:
+
+def InvokeRuntime(payload: dict) -> bool:
     """
     Invoke the runtime to process the request
     :param payload:
@@ -33,16 +35,14 @@ def InvokeRuntime(payload : dict) -> bool:
     runtime = runtimes[payload["runtime"]]
     logging.info("Invoking Runtime {}".format(runtime))
     grpc_client.run(runtime)
-    
 
     return False
-    
+
 
 if __name__ == '__main__':
     print("starting")
-    
+
     try:
         InvokeRuntime(request_payload)
     except Exception as e:
         print("Oops!", e.__class__, "occurred.")
-    
